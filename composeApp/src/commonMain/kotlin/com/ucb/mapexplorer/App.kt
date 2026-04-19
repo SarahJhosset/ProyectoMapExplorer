a3bc97105948b80cc9231ad4dca78ca0c814fedb
shalure10lure <yessicarado@gmail.com>
2026-04-19 10:11:15 -0400

    feat:añadiendo navegacion

--- composeApp/src/commonMain/kotlin/com/ucb/mapexplorer/App.kt
+++ composeApp/src/commonMain/kotlin/com/ucb/mapexplorer/App.kt
@@ -1,27 +1,29 @@
 package com.ucb.mapexplorer
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ucb.mapexplorer.dollar.presentation.screen.DollarScreen
-import org.jetbrains.compose.resources.painterResource
-
-import mapexplorer.composeapp.generated.resources.Res
-import mapexplorer.composeapp.generated.resources.compose_multiplatform
+import com.ucb.mapexplorer.navigation.AppNavHost
 
 @Composable
 @Preview
 fun App() {
-    DollarScreen()
+    val snackbarHostState = remember { SnackbarHostState() }
+    MaterialTheme(
+        //colorScheme = darkColorScheme()
+    ) {
+        Scaffold(
+            contentWindowInsets = WindowInsets.safeDrawing,
+            snackbarHost = { SnackbarHost(snackbarHostState) }
+        ) { paddingVaues ->
+            AppNavHost()
+        }
+
+    }
 }
