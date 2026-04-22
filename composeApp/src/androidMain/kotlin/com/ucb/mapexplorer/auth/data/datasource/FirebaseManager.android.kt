a2b544f8d6b99d2dad37c827a407e29db97aff63
shalure10lure <yessicarado@gmail.com>
2026-04-22 11:49:49 -0400

    feat:añadiendo librerias

--- composeApp/src/androidMain/kotlin/com/ucb/mapexplorer/auth/data/datasource/FirebaseManager.android.kt
+++ composeApp/src/androidMain/kotlin/com/ucb/mapexplorer/auth/data/datasource/FirebaseManager.android.kt
@@ -1,23 +1,16 @@
 package com.ucb.mapexplorer.auth.data.datasource
 
-import com.google.firebase.database.FirebaseDatabase
-import kotlinx.coroutines.tasks.await
-
 
 actual class FirebaseManager actual constructor() {
 
-    private val db = FirebaseDatabase.getInstance().reference
+    private val database = FirebaseDatabase.getInstance().reference
 
     actual suspend fun saveData(path: String, value: String) {
-        db.child(path).setValue(value).await()
-    }
-
-    actual suspend fun getData(path: String): String? {
-        return try {
-            val snapshot = db.child(path).get().await()
-            snapshot.getValue(String::class.java)
+        try {
+            database.child(path).setValue(value)
+            println("Firebase Android: Guardado exitoso en $path")
         } catch (e: Exception) {
-            null
+            println("Firebase Android: Error - ${e.message}")
         }
     }
 }
